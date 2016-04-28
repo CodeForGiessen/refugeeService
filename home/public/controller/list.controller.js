5e3f525be8008c272d2dc618d398ab7ada06ef11
Julian Schmitt <julian.schmitt@mni.thm.de>
2016-04-28 14:31:31 +0200

    added category list

--- home/public/controller/list.controller.js
+++ home/public/controller/list.controller.js
@@ -1,6 +1,13 @@
 (function () {
+    "use strict";
     angular.module('refugeeAuthorEnv')
-        .controller('ListController', function () {
-
-        });
+        .controller('ListController', ['$scope','CategoryCrudService', 'GuideCrudService',
+            function ($scope, CategoryCrudService, GuideCrudService) {
+                $scope.lang = window.localStorage.getItem('lang');
+                $scope.categories = {};
+                CategoryCrudService.read()
+                    .then(function (data) {
+                        $scope.categories  = data;
+                    });
+            }]);
 })();
